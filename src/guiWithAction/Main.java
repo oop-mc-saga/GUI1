2c1de6ac000a090da33609cf62fa4eeb1e1b1f69
tada195908 <48470775+tada195908@users.noreply.github.com>
2021-12-27 17:52:10 +0900

    Add files via upload

--- src/guiWithAction/Main.java
+++ src/guiWithAction/Main.java
@@ -1,11 +1,8 @@
-package guiWithAction;
+package guiWithoutAction;
 
 import java.awt.Color;
 import java.awt.Font;
-import javax.swing.JLabel;
 import javax.swing.JMenuItem;
-import javax.swing.JSlider;
-import javax.swing.BorderFactory;
 
 /**
  *
@@ -25,62 +22,18 @@ public class Main extends javax.swing.JFrame {
             return color;
         }
     }
-    private final JLabel label;
-    private final JSlider slider;
-    private final Font font;
 
     /**
      * Creates new form MainWithJMenu
      */
     public Main() {
         initComponents();
-        font = new Font("MS UI Gothic", 0, 24);
-
+        Font font = new Font("MS UI Gothic", 0, 24);
         for (Colors m : Colors.values()) {
             JMenuItem item = new JMenuItem(m.toString());
             item.setFont(font);
-            item.addActionListener(e -> colorItemPerformed(m));
             selectColors.add(item);
         }
-
-        //スライダーの生成
-        slider = new JSlider();
-        slider.setFont(font);
-        slider.setPaintTicks(true);
-        slider.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        slider.setBackground(Color.white);
-        //動作の定義
-        slider.addChangeListener(e -> sliderStateChanged(e));
-        menuBar.add(slider);//メニューバーへの追加
-
-        //ラベルの生成
-        label = new JLabel();
-        label.setFont(font);
-        menuBar.add(label);//メニューバーへの追加
-
-        int v = slider.getValue();
-        label.setText(String.valueOf(v));
-        pack();
-    }
-
-    /**
-     * 色を選択した際の動作
-     *
-     * @param c
-     */
-    private void colorItemPerformed(Colors c) {
-        System.out.println(c.toString());
-        panel.setBackground(c.getColor());
-    }
-
-    /**
-     * スライダーを操作した際の動作
-     *
-     * @param evt
-     */
-    private void sliderStateChanged(javax.swing.event.ChangeEvent evt) {
-        int v = slider.getValue();
-        label.setText(String.valueOf(v));
     }
 
     /**
@@ -92,29 +45,22 @@ public class Main extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        panel = new javax.swing.JPanel();
         menuBar = new javax.swing.JMenuBar();
         fileMenu = new javax.swing.JMenu();
         exit = new javax.swing.JMenuItem();
         selectColors = new javax.swing.JMenu();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setMinimumSize(new java.awt.Dimension(400, 200));
-        setPreferredSize(new java.awt.Dimension(400, 200));
+        setBackground(new java.awt.Color(204, 255, 204));
 
-        panel.setMinimumSize(new java.awt.Dimension(100, 100));
-        getContentPane().add(panel, java.awt.BorderLayout.CENTER);
+        menuBar.setFont(new java.awt.Font("Yu Gothic UI", 0, 24)); // NOI18N
+        menuBar.setOpaque(false);
 
         fileMenu.setText("File");
         fileMenu.setFont(new java.awt.Font("Yu Gothic UI", 0, 24)); // NOI18N
 
         exit.setFont(new java.awt.Font("Yu Gothic UI", 0, 24)); // NOI18N
-        exit.setText("exit");
-        exit.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                exitActionPerformed(evt);
-            }
-        });
+        exit.setText("Exit");
         fileMenu.add(exit);
 
         menuBar.add(fileMenu);
@@ -125,12 +71,19 @@ public class Main extends javax.swing.JFrame {
 
         setJMenuBar(menuBar);
 
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 400, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 262, Short.MAX_VALUE)
+        );
+
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    private void exitActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_exitActionPerformed
-        dispose();
-    }//GEN-LAST:event_exitActionPerformed
 
     /**
      * @param args the command line arguments
@@ -159,8 +112,6 @@ public class Main extends javax.swing.JFrame {
         }
         //</editor-fold>
         //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
@@ -174,7 +125,6 @@ public class Main extends javax.swing.JFrame {
     private javax.swing.JMenuItem exit;
     private javax.swing.JMenu fileMenu;
     private javax.swing.JMenuBar menuBar;
-    private javax.swing.JPanel panel;
     private javax.swing.JMenu selectColors;
     // End of variables declaration//GEN-END:variables
 }
